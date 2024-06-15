f8c7947085752e60eaa95e4f920b9e0c89ed8474
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2024-06-15 10:38:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const numberOfFilms = +prompt("Сколько фильмов вы смотрели?", "");
+let lookLastFilm1 = prompt("Один из последних фильмов?", "");
+let likeFilm1 = +prompt("На сколько его оцените?", "");
+let lookLastFilm2 = prompt("Один из последних фильмов?", "");
+let likeFilm2 = +prompt("На сколько его оцените?", "");
+
+const personalMuvieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+personalMuvieDB.movies[lookLastFilm1] = likeFilm1;
+personalMuvieDB.movies[lookLastFilm2] = likeFilm2;
+
+
+console.log(personalMuvieDB);
